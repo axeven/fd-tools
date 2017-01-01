1cc91e9379fe4b6efdc0393745aecea2e8c1624a
Muhammad Lathif Pambudi <axe7.mail@gmail.com>
2017-01-01 20:13:29 +0100

    handling min reward > 0

--- visualize_uct.py
+++ visualize_uct.py
@@ -129,7 +129,7 @@ def visualize_node(node, min_reward, max_reward, scale=1, hist_bucket=0.1):
             child_hist = [0] * 51
             child_x = [i * unit + 0.5 * unit for i in range(51)]
             for nd, pt in rows[i]:
-                child_hist[int(nd.reward / unit)] += len(nd.children)
+                child_hist[int((nd.reward - bins[0]) / unit)] += len(nd.children)
             plt.plot(child_x, child_hist, 'red')
         plt.grid(True)
         plt.savefig('hist_at_' + str(i) + '.png')
